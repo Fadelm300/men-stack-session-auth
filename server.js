197f0284293cf6bd5f0c03aeca61745b12563023
Fadem300 <fadel.m200@gmail.com>
2024-07-23 15:33:25 +0300

    c1

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const express = require('express');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 
+//authController import 
+const authController = require("./controllers/auth.js");
+
 require('./config/database');
+
+User = require('./models/user.js')
+const session = require('express-session');
+
 
 const app = express();
 
@@ -22,8 +29,46 @@ app.use(methodOverride('_method'));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
 
-// ROUTES
+
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+// new
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+
+// -> ROUTES
+//for authController
+app.use("/auth", authController);
+// server.js
+
+
+  
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        user:req.session.user,
+    });
+  });
+
+
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
+
+
+
+
+
+
+
+
+
+
+
